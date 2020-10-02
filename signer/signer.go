a1529226b871c722ac7b45a28b7d45a1e8837eba
Grigory <grigrol48@mail.ru>
2020-10-02 18:27:36 +0300

    fixes-refactor

--- signer/signer.go
+++ signer/signer.go
@@ -3,19 +3,21 @@ package main
 import (
 	"sort"
 	"strconv"
+	"strings"
 	"sync"
-	//"sync/atomic"
 )
 
+const capacity int = 100
+
 func ExecutePipeline(jobs ...job) {
-	input := make(chan interface{}, 100)
-	output := make(chan interface{}, 100)
+	input := make(chan interface{}, capacity)
+	output := make(chan interface{}, capacity)
 	wg := sync.WaitGroup{}
 	for _, task := range jobs {
 		wg.Add(1)
 		go worker(task, input, output, &wg)
 		input = output
-		output = make(chan interface{}, 100)
+		output = make(chan interface{}, capacity)
 	}
 	wg.Wait()
 }
@@ -26,39 +28,66 @@ func worker(task job, in, out chan interface{}, wg *sync.WaitGroup) {
 	task(in, out)
 }
 
+func JobSingleHash(data string, out chan interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
+	chF := make(chan string)
+	chS := make(chan string)
+	go func(ch chan string) {
+		val := <-ch
+		ch <- DataSignerCrc32(val)
+	}(chF)
+	go func(ch chan string) {
+		val := <-ch
+		mu.Lock()
+		data := DataSignerMd5(val)
+		mu.Unlock()
+		ch <- DataSignerCrc32(data)
+	}(chS)
+
+	chF <- data
+	chS <- data
+	first := <-chF
+	second := <-chS
+	out <- first + "~" + second
+}
+
 func SingleHash(in, out chan interface{}) {
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
 
 	for val := range in {
 		wg.Add(1)
-
 		data := strconv.Itoa(val.(int))
-		go func(out chan interface{}, wg *sync.WaitGroup, mu *sync.Mutex) {
-			defer wg.Done()
-			chF := make(chan string)
-			chS := make(chan string)
-			go func(ch chan string) {
-				val := <-ch
-				ch <- DataSignerCrc32(val)
-			}(chF)
-			go func(ch chan string) {
-				val := <-ch
-				mu.Lock()
-				data := DataSignerMd5(val)
-				mu.Unlock()
-				ch <- DataSignerCrc32(data)
-			}(chS)
-
-			chF <- data
-			chS <- data
-			first := <-chF
-			second := <-chS
-			out <- first + "~" + second
-
-		}(out, &wg, &mu)
+		go JobSingleHash(data, out, &wg, &mu)
 	}
 	wg.Wait()
+}
+
+func JobMultiHash(data string, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var channels []chan string
+	for i := 0; i < 6; i++ {
+		ch := make(chan string)
+		channels = append(channels, ch)
+	}
+
+	for i, channel := range channels {
+		go func(ch chan string, i int) {
+			val := <-ch
+			ch <- DataSignerCrc32(strconv.Itoa(i) + val)
+		}(channel, i)
+	}
+
+	for _, channel := range channels {
+		channel <- data
+	}
+
+	var result string
+	for _, ch := range channels {
+		result += <-ch
+	}
+
+	out <- result
 }
 
 func MultiHash(in, out chan interface{}) {
@@ -66,42 +95,7 @@ func MultiHash(in, out chan interface{}) {
 
 	for val := range in {
 		wg.Add(1)
-		data := val.(string)
-		go func(out chan interface{}, wg *sync.WaitGroup) {
-			defer wg.Done()
-			ch1 := make(chan string)
-			ch2 := make(chan string)
-			ch3 := make(chan string)
-			ch4 := make(chan string)
-			ch5 := make(chan string)
-			ch6 := make(chan string)
-			channels := []chan string{
-				ch1,
-				ch2,
-				ch3,
-				ch4,
-				ch5,
-				ch6,
-			}
-
-			for i, channel := range channels {
-				go func(ch chan string, i int) {
-					val := <-ch
-					ch <- DataSignerCrc32(strconv.Itoa(i) + val)
-				}(channel, i)
-			}
-
-			for _, channel := range channels {
-				channel <- data
-			}
-
-			var result string
-			for _, ch := range channels {
-				result += <-ch
-			}
-
-			out <- result
-		}(out, &wg)
+		go JobMultiHash(val.(string), out, &wg)
 	}
 	wg.Wait()
 }
@@ -112,16 +106,5 @@ func CombineResults(in, out chan interface{}) {
 		hashes = append(hashes, val.(string))
 	}
 	sort.Strings(hashes)
-
-	var result string
-	first := true
-	for _, hash := range hashes {
-		if first {
-			result += hash
-			first = false
-		} else {
-			result += "_" + hash
-		}
-	}
-	out <- result
+	out <- strings.Join(hashes, "_")
 }
